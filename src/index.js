ae5d565a0f494546cd54cd4ef5b6e00a74e746e1
JustinBreneman <Justin.Breneman@yahoo.com>
2021-08-20 14:40:26 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form")
+  let input = document.querySelector('input#new-task-description');
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(input.value);
+    addTaskToList(input.value);
+    form.reset();
+  });
 });
+
+function addTaskToList(description) {
+  const list = document.querySelector("ul#tasks");
+  let li = document.createElement('li');
+  li.innerHTML = `${description}`;
+  list.appendChild(li);
+}
